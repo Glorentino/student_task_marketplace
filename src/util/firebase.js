628bd232a4e2b42785f3ce752b67755cc2369429
Chris <86209684+Glorentino@users.noreply.github.com>
2022-10-25 16:06:19 -0400

    Removal of keys

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-      apiKey: "AIzaSyCLK4sB7Kvr5GTxF64Jz0MOLlOvEsyzqME",
-      authDomain: "tech-incubator-case-proj.firebaseapp.com",
-      databaseURL: "https://tech-incubator-case-proj-default-rtdb.firebaseio.com",
-      projectId: "tech-incubator-case-proj",
-      storageBucket: "tech-incubator-case-proj.appspot.com",
-      messagingSenderId: "560557832721",
-      appId: "1:560557832721:web:8482299a15d72e92970fb0"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     };
 
 const app = initializeApp(firebaseConfig);
